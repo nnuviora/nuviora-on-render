a1faf2491ef490383410f87a16f8160676494bb7
yaho-ma <dev76913@gmail.com>
2025-07-03 08:24:51 +0300

    docker added

--- src/api/v1/endpoints/user_profile.py
+++ src/api/v1/endpoints/user_profile.py
@@ -3,15 +3,17 @@ from fastapi.routing import APIRouter
 from fastapi import status, Depends, Request, Response, HTTPException
 from repositories.user_repo import UserRepository
 from services.user_service import UserService
-from schemas.user_schema import UserBaseSchema, UserUpdateSchema, UserChangePasswrdSchema
+from schemas.user_schema import (
+    UserBaseSchema,
+    UserUpdateSchema,
+    UserChangePasswrdSchema,
+)
 from api.v1.dependencies import get_current_user, user_dep, get_load_service
 from fastapi import UploadFile, File
 from services.load_service import LoadService
 
 
-
-
-router = APIRouter(prefix="/profile", tags=["User Profile"])
+router = APIRouter(prefix="/profile", tags=["User Profile33"])
 
 
 user_service_dep = Annotated[UserService, Depends(user_dep)]
@@ -30,6 +32,7 @@ async def profile(user: user_base_schema_dep) -> UserBaseSchema:
     return user
 
 
+
 @router.delete(
     "/delete_user",
     status_code=status.HTTP_204_NO_CONTENT,
@@ -41,8 +44,7 @@ async def profile(user: user_base_schema_dep) -> UserBaseSchema:
     },
 )
 async def delete_user(
-    current_user: user_base_schema_dep, 
-    user_service: user_service_dep
+    current_user: user_base_schema_dep, user_service: user_service_dep
 ):
 
     user_id = current_user.get("id")
@@ -68,9 +70,7 @@ async def update_user_info(
     new_data: UserUpdateSchema,
     user_service: user_service_dep,
 ):
-    new_data = new_data.model_dump(
-        exclude_none=True
-    )
+    new_data = new_data.model_dump(exclude_none=True)
     updated_user = await user_service.update_user(
         user_id=dep["id"], update_data=new_data
     )
@@ -78,8 +78,9 @@ async def update_user_info(
 
 
 @router.put(
-    "/change_password", status_code=status.HTTP_200_OK,
-        responses={
+    "/change_password",
+    status_code=status.HTTP_200_OK,
+    responses={
         200: {"description": "Пароль оновлено успішно"},
         400: {"description": "Неправильний запит"},
         401: {"description": "Немає прав доступу"},
@@ -92,7 +93,7 @@ async def update_user_info(
 async def update_user_password(
     current_user: user_base_schema_dep,
     user_service: user_service_dep,
-    new_password: UserChangePasswrdSchema
+    new_password: UserChangePasswrdSchema,
 ):
     """
     Update the password for the current authenticated user.
@@ -108,32 +109,34 @@ async def update_user_password(
     Returns:
         dict: A success message indicating the password was updated.
     """
-    
+
     is_valid = await user_service.security_layer.verify_password(
         password=new_password.current_password,
-        hash_password=current_user["hash_password"]
+        hash_password=current_user["hash_password"],
     )
     if not is_valid:
         raise HTTPException(status_code=403, detail="Неправильний поточний пароль")
-    
 
-
-    
     user_with_new_pass = await user_service.change_user_password(
         user_id=current_user["id"],
-        new_password={"new_password": new_password.new_password}
+        new_password={"new_password": new_password.new_password},
     )
-    #DELETE print statement IN PRODUCTION
-    print (f"Passwoprd for user {user_with_new_pass.get('email')} was updated succesfully")
-    return {"message": f"Пароль для користувача: {current_user.get('id')} успішно оновлений"}
+    # DELETE print statement IN PRODUCTION
+    print(
+        f"Passwoprd for user {user_with_new_pass.get('email')} was updated succesfully"
+    )
+    return {
+        "message": f"Пароль для користувача: {current_user.get('id')} успішно оновлений"
+    }
+
 
 ###########################################################################################
-#AVATAR UPLOAD AND GET
+# AVATAR UPLOAD AND GET
 ###########################################################################################
 
 
 @router.post(
-    "/upload-avatar", 
+    "/upload-avatar",
     status_code=status.HTTP_200_OK,
     summary="Upload user avatar to S3",
     responses={
@@ -148,7 +151,11 @@ async def upload_avatar(
     user_service: UserService = Depends(user_dep),
     load_service: LoadService = Depends(get_load_service),
 ) -> dict:
-    user_id = current_user.get("id") if isinstance(current_user, dict) else getattr(current_user, "id", None)
+    user_id = (
+        current_user.get("id")
+        if isinstance(current_user, dict)
+        else getattr(current_user, "id", None)
+    )
     if not user_id:
         raise HTTPException(status_code=400, detail="Invalid user data")
 
@@ -160,11 +167,12 @@ async def upload_avatar(
 
     return {"avatar_url": avatar_url}
 
+
 ###########################################################################################
 
 
 @router.get(
-    "/get-avatar", 
+    "/get-avatar",
     status_code=status.HTTP_200_OK,
     summary="Get user avatar from S3",
     responses={
@@ -176,15 +184,14 @@ async def upload_avatar(
 async def get_avatar(
     current_user: user_base_schema_dep,
     user_service: user_service_dep,
-    ):
+):
     user_id = current_user.get("id")
     if not user_id:
         raise HTTPException(status_code=400, detail="Invalid user data")
-    
-    
+
     all_info_about_curr_user = await user_service.get_one_user(user_id=user_id)
     avatar_url_for_curr_user = all_info_about_curr_user.get("avatar")
     if not avatar_url_for_curr_user:
         raise HTTPException(status_code=400, detail="Avatar not found")
-    
+
     return {"avatar_url": avatar_url_for_curr_user}
